074509595bd332133b7c8868816bf73b85d6a7f0
Ruban_p <rubansmash17@gmail.com>
2023-01-29 15:41:00 +0530

    first1

--- src/App.js
+++ src/App.js
@@ -23,29 +23,7 @@ export default function App() {
           <input type="text" value={value} onChange={onChange} />
           <button onClick={() => onSearch(value)}> Search </button>
         </div>
-        {/* <div className="dropdown">
-          {data
-            .filter((item) => {
-              const searchTerm = value.toLowerCase();
-              const fullName = item.full_name.toLowerCase();
-
-              return (
-                searchTerm &&
-                fullName.startsWith(searchTerm) &&
-                fullName !== searchTerm
-              );
-            })
-            .slice(0, 10)
-            .map((item) => (
-              <div
-                onClick={() => onSearch(item.full_name)}
-                className="dropdown-row"
-                key={item.full_name}
-              >
-                {item.full_name}
-              </div>
-            ))}
-        </div> */}
+        
       </div>
     </div>
   );
